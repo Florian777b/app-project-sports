4bb88c715c33894cefec618c76447edc8a01a14e
Maxim <werkhowski@gmail.com>
2021-03-03 11:22:10 +0100

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -15,12 +15,20 @@ function App() {
     const access_key = "9651c1a2da7441a8ab810b656d815648";
     const APIlink = `https://api.sportsdata.io/v3/nhl/scores/json/AllTeams`
   
-    useEffect(async () => {
-          const response = await fetch(APIlink,{"Ocp-Apim-Subscription-Key": access_key
-          })
-          const resData = await response.json()
-          setData(resData.results)
-      }, [])
+    async function getScores() {
+      const response = await fetch(APIlink,{
+        headers: {
+          "Ocp-Apim-Subscription-Key": access_key
+        }
+        
+      })
+      const resData = await response.json()
+      setData(resData.results)
+    }
+
+    useEffect(() => {
+      getScores()
+    }, [])
        
 
  
